d66bea5e301ad331abe4a66f05478aa33c049d0c
Eren Dalçık <dalcikferhateren@gmail.com>
2023-11-05 18:21:08 +0300

    add networkdata code

---
+++ Assets/_Project/Scripts/Runner/NetworkData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runner
+{
+    [CreateAssetMenu(fileName = "NetworkData", menuName = "NetworkData/Network")]
+    public class NetworkData : ScriptableObject
+    {
+        public string apiKey = "";
+    }
+}
