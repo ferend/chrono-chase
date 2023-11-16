026c67aae17d0982f7e30f7010d705d4086e7770
Eren Dalçık <dalcikferhateren@gmail.com>
2023-11-16 12:51:52 +0300

    input control with new input system

---
+++ Assets/_Project/Scripts/Runner.Game/Input/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets._Project.Scripts.Runner.Game.Input
+{
+    public class InputManager : MonoBehaviour
+    {
+        // Adjust this speed to control the movement speed
+        public float moveSpeed = 5f;
+        public Swipe swipe;
+        public Rigidbody rb;
+        
+        private void Start()
+        {
+            swipe.OnSwipe += OnSwiped;
+        }
+
+
+        private void OnSwiped(Swipe.GestureData pos)
+        {
+            rb.velocity = pos.rawDirection.normalized * moveSpeed;
+        }
+
+     
+    }
+}
---
+++ Assets/_Project/Scripts/Runner.Game/Input/Swipe.cs
@@ -0,0 +1,124 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+using UnityEngine.XR;
+
+public enum InputType
+{
+    Button = 0,
+    Swipe = 1,
+    All = 2
+}
+
+namespace Assets._Project.Scripts.Runner.Game.Input
+{
+    [CreateAssetMenu(menuName = "Create Swipe", fileName = "Swipe", order = 0)]
+    public class Swipe : ScriptableObject
+
+    {
+        [Serializable]
+        public struct GestureData
+        {
+            public Vector2Int clampedDirection;
+            public Vector2 rawDirection;
+            public float duration;
+            public Vector2 _initialPosition;
+
+            public GestureData(Vector2Int clampedDirection, Vector2 rawDirection, float duration, Vector2 initialPosition)
+            {
+                this.clampedDirection = clampedDirection;
+                this.rawDirection = rawDirection;
+                this.duration = duration;
+                _initialPosition = initialPosition;
+            }
+        }
+
+        public event Action<GestureData> OnSwipe;
+
+        [Space]
+        [SerializeField] private float minDuration;
+        [SerializeField] private float minDistance;
+        [SerializeField] private float maxDistance;
+        [SerializeField] private float deadZone;
+
+
+        private Vector2 _traveled;
+        private Vector2Int _lastDirection;
+        private float _startTime;
+        private Vector2 _startPosition;
+
+        private PlayerInput _input;
+
+        private void OnEnable()
+        {
+            if(_input == null)
+            {
+                _input = new PlayerInput();
+            }
+            
+            _input.PlayerControls.Enable();
+
+
+            _input.PlayerControls.OnPrimaryTouch.started += TouchStart;
+            _input.PlayerControls.OnPrimaryTouch.canceled += TouchEnd;
+            _input.PlayerControls.PrimaryTouchPosition.performed += TouchStartPosition;
+            _input.PlayerControls.TouchDelta.started += TouchUpdate;
+
+        }
+
+        private void OnDisable()
+        {
+            if(_input!= null) _input.PlayerControls.Disable();
+
+                 
+        }
+
+
+        private void TouchStart(InputAction.CallbackContext context)
+        {
+            _traveled = Vector2.zero;
+            _startTime = Time.time;
+
+        }
+
+        private void TouchStartPosition(InputAction.CallbackContext context)
+        {
+            
+               _startPosition = context.ReadValue<Vector2>();
+            
+        }
+
+        private void TouchUpdate(InputAction.CallbackContext context)
+        {
+            _traveled += context.ReadValue<Vector2>();
+        }
+
+        private void TouchEnd(InputAction.CallbackContext context)
+        {
+            float duration = Time.time - _startTime;
+
+            if (duration < minDuration )
+            {
+                return;
+            }
+
+            float d = Mathf.Abs(_traveled.normalized.x) - Mathf.Abs(_traveled.normalized.y);
+
+            if (d < -deadZone)
+            {
+                _lastDirection = new Vector2Int(0, (int)(Mathf.Ceil(Mathf.Abs(_traveled.normalized.x)) * Mathf.Sign(_traveled.normalized.y)));
+                OnSwipe?.Invoke(new GestureData(_lastDirection, _traveled, duration, _startPosition));
+
+                return;
+            }
+
+            if (d > deadZone)
+            {
+                _lastDirection = new Vector2Int((int)(Mathf.Ceil(Mathf.Abs(_traveled.normalized.x)) * Mathf.Sign(_traveled.normalized.x)), 0);
+                OnSwipe?.Invoke(new GestureData(_lastDirection, _traveled, duration, _startPosition));
+            }
+        }
+
+    }
+}
